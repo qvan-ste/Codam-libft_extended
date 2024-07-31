c57dd79946bee2f27271b0b7bd460db638943b2c
Quincy Van Stenus <qvan-ste@f1r4s7.codam.nl>
2024-07-31 16:11:39 +0200

    extra bugfixes to vector insert

--- src/libft/vectors/ft_vecinsert_ptr.c
+++ src/libft/vectors/ft_vecinsert_ptr.c
@@ -6,7 +6,7 @@
 /*   By: qvan-ste <qvan-ste@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/17 16:16:43 by qvan-ste      #+#    #+#                 */
-/*   Updated: 2024/07/31 12:08:45 by qvan-ste      ########   odam.nl         */
+/*   Updated: 2024/07/31 12:52:01 by qvan-ste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,20 @@ int	ft_vecinsert_ptr(t_vector_ptr *vec, void *item, size_t index)
 {
 	size_t i;
 
-	i = index;
-	if (index >= vec -> size)
-		return (0);
-	if (index < vec -> size - 1)
+	i = vec -> size;
+	if (!vec || !item || index >= vec -> size)
+		return (-1);
+	if (vec -> size == vec -> capacity)
 	{
-		while (i < vec -> size - 1)
-		{
-			vec -> items[i] = vec -> items [i + 1];
-			i++;
-		}
-		ft_vecset_ptr(vec, item, index);
-		vec -> size++;
-	}
-	else
-	{
-		if (ft_vecappend_ptr(vec, item) == -1)
+		if (ft_vecresize_ptr(vec, vec -> capacity * 2) == -1)
 			return (-1);
 	}
+	while (i > index)
+	{
+		vec -> items[i] = vec -> items [i - 1];
+		i--;
+	}
+	vec -> items[index] = item;
+	vec -> size++;
 	return (0);
 }
