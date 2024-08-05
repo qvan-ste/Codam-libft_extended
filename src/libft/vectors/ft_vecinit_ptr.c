48884371fd6f7c0e6558978385738be38038933c
Quincy Van Stenus <qvan-ste@f1r4s7.codam.nl>
2024-08-05 18:44:54 +0200

    added nullpointer for vec_ptr

--- src/libft/vectors/ft_vecinit_ptr.c
+++ src/libft/vectors/ft_vecinit_ptr.c
@@ -6,7 +6,7 @@
 /*   By: qvan-ste <qvan-ste@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/17 15:58:52 by qvan-ste      #+#    #+#                 */
-/*   Updated: 2024/07/29 14:38:15 by qvan-ste      ########   odam.nl         */
+/*   Updated: 2024/08/05 16:27:08 by qvan-ste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_vecinit_ptr(t_vector_ptr *vec, size_t capacity, size_t item_size)
 	vec -> item_size = item_size;
 	vec -> capacity = capacity;
 	vec -> size = 0;
-	vec -> items = ft_calloc(vec -> capacity, item_size);
+	vec -> items = ft_calloc(vec -> capacity + 1, item_size);
 	if (!vec -> items)
 		return (-1);
 	return (0);
